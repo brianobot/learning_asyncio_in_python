54a1b96f210f2b55f130eb30ffc50a52ecaf7dd5
Brian Obot <brianobot9@gmail.com>
2023-09-05 10:50:26 +0100

    Update codebase

--- 1. generators/intro.py
+++ 1. generators/intro.py
@@ -21,3 +21,39 @@ to it again.
 """
 
 
+"""
+GENERATORS.
+
+Unlike normal functions that when called are ran from top to bottom and optional return a value,
+the generator function return their value through a yield statement everything it is called by a next function
+directly or indirectly through a for loop.
+
+Example below
+"""
+
+def normal_function():
+    # does some action in the function body
+    # nothing interesting to see here, lets move on to return a value
+    return 12
+
+
+result = normal_function() # the function is called and the result value is assigned to a variable
+print(f"\nthe return value of the {normal_function.__name__} is {result}")
+
+
+def generator_function():
+    # function body 
+    yield 1
+    yield 2
+    yield 3
+
+# in order to get the values from the generator functions, lets use a for loop
+for item in generator_function():
+    print(item)
+
+"""
+Now to the function presented above, it's a typical function declaration and calling process,
+for most common functions the function starts and returns almost immediately ... and the other part
+of the program is ran ..but some functions have to do some waiting for some external resources, like download
+some data from the web, or read data from a file, or write data into a file.
+"""
