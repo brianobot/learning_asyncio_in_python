4416cea233cba1adbd8861f4159920d5c5179ea6
Brian Obot <brianobot9@gmail.com>
2023-09-05 16:00:40 +0100

    started intro on coroutines

---
+++ 1. generators/key_note.py
@@ -0,0 +1,14 @@
+"""
+This highlights the top level ideas covered in this directory
+"""
+
+
+"""
+* When generator functions are called, they do not return a value like other functions directly
+but return a generator object which implement the iterator protocol and can be directly or indirectly
+with the next function to obtain it values.
+
+
+* This ability to go to sleep and resume from where it stop it due to this functions ability
+to its state and it stack frame.
+"""
---
+++ 2. coroutines/intro.py
@@ -0,0 +1,5 @@
+"""
+Having been introduced to the concepts and syntax around the generator functions,
+which when called returns a generator object which then can be ran and paused 
+according to the distribution of yield statements in the function's body.
+"""
