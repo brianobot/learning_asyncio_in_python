be911c1974b9cd0360e4871355a0b6ba57c67944
Brian Obot <brianobot9@gmail.com>
2023-09-08 14:25:39 +0100

    Update codebase

---
+++ 3. async and await/intro.py
@@ -0,0 +1,14 @@
+"""
+In All Modern OS, there are a list of processes running on the computer system (performing some action).
+Now since the processes are usually much more the available processors, the OS has to periodically
+suspend execution of some processes for other to have their own fair share of time to execute, the 
+OS knows how to suspend this process, and how to restart such that the process continues from where it 
+stopped. this form is multitasking is called pre-emptive.
+
+Now this idea is essential and applicable to the asyncio ecosystem, but unlike the operating system
+which handles the switching, the event loop, which is an infinitely running while loop switches between
+different events (functions) allowing each to execute and willingly give up control when sleeping and waiting
+for some input, output.
+
+
+"""
