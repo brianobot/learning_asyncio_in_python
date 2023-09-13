cd7225f4ae186fde2fd7b4f9f9628f97d1edb5ce
Brian Obot <brianobot9@gmail.com>
2023-09-13 17:55:42 +0100

    Update lesson process

--- 4. event_loops/intro.py
+++ 4. event_loops/intro.py
@@ -13,3 +13,24 @@ loop  = asyncio.get_event_loop()
 # since they can only be a single event loop in the system at a given time (its a singleton)
 
 # so invoking get_event_loop mutiple times would always give the same event loop
+
+# once gotten, the event loop can be started with the run_forever method
+loop.run_forever()
+
+"""
+now what we have done works, the event loop starts and actually runs until stop() is called
+
+but there are 2 problems
+- first, it get to run forever
+- and then not task has been place in the loop, which is kind of the whole point of the loop
+"""
+
+# inorder to make use of the event loop we need to write a coroutine, 
+# this is more like  regular function only that it is defined with the async key word infront
+# the normal function def keyword.
+
+
+async def greet():
+    print("Hello World!")
+
+    
